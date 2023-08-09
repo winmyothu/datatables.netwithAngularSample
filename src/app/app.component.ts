ca664ded4c3d3d879ddfb0aaf13dd113e559d0c3
Win Myo Thu <winmyothu@myanmareasenet.com>
2023-08-09 14:53:40 +0630

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-new-app';
+  title = 'datatables';
+  dtOptions: DataTables.Settings = {};
+  posts: any;
+  @ViewChild(DataTableDirective) dtElement: DataTableDirective | undefined;
+  dtTrigger: Subject<any> = new Subject();
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pageLength: 10,
+      responsive: true,
+      dom: 'Bfrtip',
+      order: [],
+      columnDefs: [{ targets: 0, orderable: false }],
+      // buttons: [
+      //   {
+      //     className: '',
+      //     extend: 'excel',
+      //     sheetName: 'Company Code',
+      //     title: 'Company Code List',
+      //     exportOptions: {
+      //       columns: [1, 2, 3],
+      //       modifier: {
+      //         page: 'current',
+      //       },
+      //     },
+      //   },
+      //   {
+      //     extend: 'print',
+      //     exportOptions: {
+      //       columns: [1, 2, 3],
+      //     },
+      //   },
+      // ],
+    };
+
+    this.http
+      .get('http://jsonplaceholder.typicode.com/posts')
+      .subscribe((posts) => {
+        this.posts = posts;
+        this.rerender();
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.dtTrigger.next(null);
+  }
+
+  rerender(): void {
+    this.dtElement?.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.destroy();
+      this.dtTrigger.next(null);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataTablesModule } from 'angular-datatables';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
